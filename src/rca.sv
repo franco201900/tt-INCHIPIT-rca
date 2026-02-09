70076b2f4611dc9ab773190e2f114d0ecc5fa251
Francescomoroni2008 <francesco.moroni2008@gmail.com>
2026-02-09 16:18:29 +0100

    Complete for loop range and add missing logic

--- src/rca.sv
+++ src/rca.sv
@@ -14,7 +14,7 @@ module rca
 
     genvar i;
     generate;
-        for(i=0; i< /*Completare qui*/; i=i+1) begin: gen_rca 
+        for(i=0; i<3; i=i+1) begin: gen_rca 
             if (i == 0) begin: gen_0
                 fa_module u(
                     .a(a[i]),
@@ -24,7 +24,7 @@ module rca
                     .cout(carry[i])
                 );
             end else begin: gen_gt0
-                /* Completare qui. */
+            
 
             end
         end
